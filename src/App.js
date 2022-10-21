3a32ce884a517616b6344748b07ac534f548083d
Mark Tucker <markdaddy@Marks-MacBook-Air.local>
2022-10-21 15:52:43 -0600

    data pulled and generic page setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
+
+  const [nasaData, setNasaData] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(res => {
+        console.log(res.data);
+        setNasaData(res.data)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>Nasa Photo of the Day for {nasaData.date}!</h1>
+        <img src={nasaData.url} />
+        <h2>{nasaData.title}</h2>
+        <p>
+        <span role="img" aria-label='go!'>🚀</span> {nasaData.explanation} <span role="img" aria-label='go!'>🚀</span>
+        </p>
+      <footer>
+          <h3>Copyright: {nasaData.copyright}</h3>
+      </footer>
+
     </div>
   );
 }
+
 
 export default App;
